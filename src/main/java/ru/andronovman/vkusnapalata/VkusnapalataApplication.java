ac64ca0dadf8a1cf1ec851af6b9dc4c1b7b5efea
andronovman <andronovman@mail.ru>
2023-03-16 09:30:29 +0400

    добавил проектную документацию (ПД)

---
+++ src/main/java/ru/andronovman/vkusnapalata/VkusnapalataApplication.java
@@ -0,0 +1,13 @@
+package ru.andronovman.vkusnapalata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VkusnapalataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VkusnapalataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/andronovman/vkusnapalata/VkusnapalataApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.andronovman.vkusnapalata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VkusnapalataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
